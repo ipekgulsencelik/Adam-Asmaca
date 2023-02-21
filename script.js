05ee6aa60b2b21a23d1120951177ca45e2123144
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-02-21 23:33:41 +0300

    showing word

---
+++ script.js
@@ -0,0 +1,29 @@
+const word_element = document.getElementById("word");
+
+const correctLetters = ["j", "a", "v", "s", "t"];
+const wrongLetters = [];
+
+function getRandomWord() {
+  const words = ["javascript", "java", "python"];
+
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function displayWord() {
+  const selectedWord = getRandomWord();
+
+  word_element.innerHTML = `
+        ${selectedWord.split("").map((letter) => `
+            <div class="letter">
+                ${correctLetters.includes(letter) ? letter : ""}
+            </div>
+        `).join("")}
+    `;
+
+  const enteredWord = word_element.innerText.replace(/\n/g, "");
+  if (enteredWord === selectedWord) {
+    console.log("bildiniz.");
+  }
+}
+
+displayWord();
